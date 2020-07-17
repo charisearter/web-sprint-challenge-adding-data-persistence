4b816d613321f0132749e7b442d85d87f20c700c
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-17 13:59:43 -0500

    migrate not working

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-//server stuff goes here after migrations and seeds set
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
